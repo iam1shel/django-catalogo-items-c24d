21831dc3bbe9165228bfb84ae018951d812339d3
iam1shel <luzmishelrt13@gmail.com>
2026-08-22 20:14:04 -0500

    Agregar API de Items y consumo con JavaScript

--- src/core/urls.py
+++ src/core/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.item_list, name='item_list'),
+    path('api/items/', views.item_api, name='item_api'),
 ]
--- src/core/views.py
+++ src/core/views.py
@@ -1,8 +1,23 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 from .models import Item
+
 
 def item_list(request):
     items = Item.objects.all()
     return render(request, 'core/item_list.html', {'items': items})
 
-# Create your views here.
+
+def item_api(request):
+    items = Item.objects.all()
+
+    data = [
+        {
+            'id': item.id,
+            'name': item.name,
+            'description': item.description
+        }
+        for item in items
+    ]
+
+    return JsonResponse(data, safe=False)
